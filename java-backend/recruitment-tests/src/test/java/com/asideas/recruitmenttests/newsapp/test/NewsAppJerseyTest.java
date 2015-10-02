3bb1bd1a28e4d8d3bcc5835ab5ecacc51f3f6ff2
noacktino <noacktin@b-tu.de>
2015-10-02 16:23:34 +0200

    changed unit tests

--- java-backend/recruitment-tests/src/test/java/com/asideas/recruitmenttests/newsapp/test/NewsAppJerseyTest.java
+++ java-backend/recruitment-tests/src/test/java/com/asideas/recruitmenttests/newsapp/test/NewsAppJerseyTest.java
@@ -1,11 +1,11 @@
 package com.asideas.recruitmenttests.newsapp.test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.UUID;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Assert;
@@ -16,34 +16,49 @@ import com.asideas.recruitmenttests.newsapp.pojo.NewsPojo;
 
 public class NewsAppJerseyTest extends JerseyTest {
 	
-	private String newsXML = "";
-	private String newsJSON = ""; 
-	
 	@Override
 	public Application configure() {
-		try {
-			newsXML = new String(Files.readAllBytes(
-					Paths.get(getClass().getResource("./news.xml").toURI())));
-			newsJSON = new String(Files.readAllBytes(
-					Paths.get(getClass().getResource("./news.json").toURI())));
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
 		return new NewsApplication();
 	}
 	
 	@Test
-	public void insertNews() {
-		Assert.assertEquals(
-				target("v1/news").request(MediaType.APPLICATION_JSON).post(
-						Entity.entity(newsJSON, MediaType.APPLICATION_JSON))
-						.getStatus(), 200);
+	public void getAllNews() {
+		Assert.assertEquals(target("v1/news").request().get().getStatus(), 200);
 	}
 	
 	@Test
-	public void getNews() {
-		Assert.assertEquals(target("v1/news").request().get().getStatus(), 200);
-		target("v1/news/5731d173-063f-4921-99dd-9e417d64171a").request().get().readEntity(NewsPojo.class);
+	public void getSpecificNewsEntry() {
+		Assert.assertEquals(
+				target("v1/news/5731d173-063f-4921-99dd-9e417d64171a")
+				.request().get().getStatus(), 200);
+	}
+	
+	@Test
+	public void deleteSpecificNewsEntry() {
+		Assert.assertEquals(
+				target("v1/news/071c74c2-1885-4739-8690-af9907afb977")
+				.request().delete().getStatus(), 200);		
+	}
+	
+	@Test
+	public void putSpecificNewsEntry() {
+		NewsPojo pojo = new NewsPojo();
+		pojo.setHeader("Test Header");
+		pojo.setShortDescription("Test Description");
+		pojo.setId(UUID.fromString("5731d173-063f-4921-99dd-9e417d64171a"));
+	    Response res = target("v1/news/5731d173-063f-4921-99dd-9e417d64171a")
+	    		.request().put(Entity.entity(pojo, MediaType.APPLICATION_JSON));
+		Assert.assertEquals(200, res.getStatus());				
+	}
+	
+	@Test
+	public void insertNews() {
+		NewsPojo pojo = new NewsPojo();
+		pojo.setHeader("Test Header");
+		pojo.setShortDescription("Test Description");
+	    Response res = target("v1/news").request().post(
+	    		Entity.entity(pojo, MediaType.APPLICATION_JSON));
+		Assert.assertEquals(200, res.getStatus());
 	}
 	
 }
